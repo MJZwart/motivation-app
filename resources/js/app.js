fd8bd154faae8653d33418a4b57f9d02f53b40a1
MJZwart <prorenata.blue@gmail.com>
2022-04-11 11:39:58 +0200

    Moved icons to separate file and fixed warn

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,12 @@
-// import Vue from 'vue';
-    
-// import toastService from './services/toastService';
-// const app = new Vue({
-//     i18n,
-//     el: '#app',
-//     store,
-//     router,
-//     render: h => h(App),
-// });
-        
+import './bootstrap';
+import './interceptors';
+import '../assets/scss/app.scss';
+      
 // //Import store
 import {store} from './store/store';
         
 // //Import router
 import router from './router/router';
-        
-import './bootstrap';
-import './interceptors';
-import '../assets/scss/app.scss';
         
 import {createApp} from 'vue';
 import App from './App.vue';
@@ -27,56 +16,14 @@ const app = createApp(App);
 app.use(store);
 app.use(router);
 
-
 //Import vue-i18n translations
 import i18n from './i18n';
 app.use(i18n);
-
-
 
 //Font-awesome icons
 import {FontAwesomeIcon, FontAwesomeLayers} from '@fortawesome/vue-fontawesome';
 app.component('FaIcon', FontAwesomeIcon);
 app.component('FaIconLayers', FontAwesomeLayers);
-
-import {library} from '@fortawesome/fontawesome-svg-core';
-//Regular icons
-import {
-    faPenToSquare, 
-    faFlag, 
-    faBell, 
-    faSquareCheck,
-    faCirclePlay,
-    faRectangleXmark,
-} from '@fortawesome/free-regular-svg-icons';
-//Solid icons
-import {
-    faTrash, 
-    faSquarePlus, 
-    faBan, 
-    faPlus,
-    faEnvelope, 
-    faEnvelopeOpen,
-    faCircle,
-    faArrowTurnUp,
-    faUserPlus,
-} from '@fortawesome/free-solid-svg-icons';
-library.add(
-    faPenToSquare, 
-    faTrash, 
-    faEnvelope, 
-    faSquarePlus, 
-    faBan, 
-    faFlag, 
-    faBell, 
-    faSquareCheck, 
-    faPlus,
-    faEnvelopeOpen,
-    faCircle,
-    faArrowTurnUp,
-    faUserPlus,
-    faCirclePlay,
-    faRectangleXmark,
-);
+import './icons.js';
 
 app.mount('#app');
