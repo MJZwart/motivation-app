30eb0281b03b1959c0e88fd8052095d288abbf78
MJZwart <prorenata.blue@gmail.com>
2022-05-02 10:29:57 +0200

    Login and css

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -2,27 +2,22 @@
     <div class="w-40 center">
         <h2>{{ $t('login') }}</h2>
         <form @submit.prevent="submitLogin">
-            <div class="form-group">
-                <label label-for="username">{{$t('username')}}</label>
-                <input 
-                    id="username" 
-                    v-model="login.username" 
-                    type="text" 
-                    :placeholder="$t('username')" />
-                <base-form-error name="username" /> 
-            </div>
-            <div class="form-group">
-                <label label-for="password">{{$t('password')}}</label>
-                <input  
-                    id="password" 
-                    v-model="login.password"
-                    type="password" 
-                    name="password" 
-                    :placeholder="$t('password')" />
-                <base-form-error name="password" /> 
-            </div>
+            <Input 
+                id="username" 
+                v-model="login.username" 
+                name="username"
+                type="text" 
+                :label="$t('password')"
+                :placeholder="$t('username')" />
+            <Input  
+                id="password" 
+                v-model="login.password"
+                type="password" 
+                name="password" 
+                :label="$t('password')"
+                :placeholder="$t('password')" />
             <button type="submit" class="block">{{ $t('login') }}</button>
-            <base-form-error name="error" /> 
+            <BaseFormError name="error" /> 
         </form> 
     </div>
 </template>
