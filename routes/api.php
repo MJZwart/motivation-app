fd3b6473a3b7f727878a987db37c5cb3449df64a
LaEve <la.eve@mailbox.org>
2022-05-10 16:53:57 +0200

    added request join to routes

--- routes/api.php
+++ routes/api.php
@@ -92,6 +92,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::put('/conversation/{conversation}/read', [MessageController::class, 'markConversationAsRead']);
 
     Route::post('/groups/join/{group}', [GroupsController::class, 'join']);
+    Route::post('groups/requestjoin/{group}', [GroupsController::class, 'requestJoin']);
     Route::post('/groups/leave/{group}', [GroupsController::class, 'leave']);
     Route::resource('/groups', GroupsController::class)->only([
         'store', 'destroy', 'show',
