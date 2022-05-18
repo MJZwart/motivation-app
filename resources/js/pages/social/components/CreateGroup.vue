2e5bf5eee15c2b28e31159f18a487413f9a39922
LaEve <la.eve@mailbox.org>
2022-05-18 11:41:44 +0200

    fixed input+baseformerror names

--- resources/js/pages/social/components/CreateGroup.vue
+++ resources/js/pages/social/components/CreateGroup.vue
@@ -20,22 +20,22 @@
                     id="public-checkbox"
                     v-model="groupToCreate.is_public"
                     type="checkbox"
-                    name="public-checkbox" />
+                    name="is_public" />
                 <label for="public-checkbox" class="option-label">{{$t('group-public-checkbox')}}</label>
                 <small class="form-text text-muted">{{$t('group-public-checkbox-desc')}}</small>
-                <BaseFormError name="public-checkbox" /> 
+                <BaseFormError name="is_public" /> 
             </div>
             <div v-if="groupToCreate.is_public" class="form-group">
                 <input
                     id="require-application-checkbox"
                     v-model="groupToCreate.require_application"
                     type="checkbox"
-                    name="require-application-checkbox" />
+                    name="require_application" />
                 <label for="require-application-checkbox" class="options-label">
                     {{$t('group-require-application-checkbox')}}
                 </label>
                 <small class="form-text text-muted">{{$t('group-require-application-checkbox-desc')}}</small>
-                <BaseFormError name="require-application-checkbox" />
+                <BaseFormError name="require_application" />
             </div>
             <button type="submit" class="block">{{$t('create-group')}}</button>
             <button type="button" class="block" @click="close">{{$t('cancel')}}</button>
