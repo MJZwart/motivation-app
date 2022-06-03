bd7874bb8e11afc572792c564c03679d691308dd
MJZwart <prorenata.blue@gmail.com>
2022-06-03 10:33:31 +0200

    Moved translations

--- resources/js/pages/ErrorPage.vue
+++ resources/js/pages/ErrorPage.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="d-flex">
         <div class="w-50-flex center">
-            <h2>Page not found</h2>
-            <p>If you believe this is an error, please submit a <router-link to="/bugreport">bug report</router-link>.</p>
-            <p>Go back <router-link to="/">home</router-link>.</p>
+            <h2>{{ $t('page-not-found') }}</h2>
+            <p>{{ $t('if-mistake-submit-report')}} <router-link to="/bugreport">{{ $t('bug-report-lc') }}</router-link>.</p>
+            <p>{{ $t('go-back') }} <router-link to="/">{{ $t('home-lc') }}</router-link>.</p>
         </div>
     </div>
 </template>
