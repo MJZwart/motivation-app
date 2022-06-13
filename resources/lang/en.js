2a884adeaa660661d86d6cfb35413c814e945e59
MJZwart <prorenata.blue@gmail.com>
2022-06-13 10:57:54 +0200

    Changes to translation files

--- resources/lang/en.js
+++ resources/lang/en.js
@@ -1,11 +1,21 @@
-import general from './general/en.json';
-import tutorial from './tutorial/en.json';
-import time from './time/en.json';
-import group from './group/en.json';
+import general from './i18n/general/en.json';
+import tutorial from './i18n/tutorial/en.json';
+import time from './i18n/time/en.json';
+import group from './i18n/group/en.json';
+import task from './i18n/task/en.json';
+import reward from './i18n/reward/en.json';
+import admin from './i18n/admin/en.json';
+import friend from './i18n/friend/en.json';
+import message from './i18n/message/en.json';
 
 export default {
     ...general,
     ...tutorial,
     ...time,
     ...group,
+    ...task,
+    ...reward,
+    ...admin,
+    ...friend,
+    ...message,
 }
