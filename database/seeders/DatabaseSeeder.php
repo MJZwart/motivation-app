bb4065f976c13345555b4615220b61e4c5219c78
MJZwart <prorenata.blue@gmail.com>
2022-08-15 13:50:52 +0200

    Achievement names in seeder and split by env

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,19 +16,24 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ExampleTaskSeeder::class,
-            UserSeeder::class,
-            TaskSeeder::class,
-            NotificationSeeder::class,
-            FriendSeeder::class,
             AchievementSeeder::class,
             ExperiencePointsSeeder::class,
             BalancingSeeder::class,
-            MessageSeeder::class,
-            BugReportSeeder::class,
-            GroupSeeder::class,
-            FeedbackSeeder::class,
-            ReportedUserSeeder::class,
-            BannedUsersSeeder::class,
         ]);
+        if (App::environment(['local', 'testing', 'staging'])) {
+            $this->call([
+                UserSeeder::class,
+                TaskSeeder::class,
+                NotificationSeeder::class,
+                FriendSeeder::class,
+                MessageSeeder::class,
+                BugReportSeeder::class,
+                GroupSeeder::class,
+                FeedbackSeeder::class,
+                ReportedUserSeeder::class,
+                BannedUsersSeeder::class,
+                AchievementEarnedSeeder::class,
+            ]);
+        }
     }
 }
