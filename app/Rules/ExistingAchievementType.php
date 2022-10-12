425f7632a8f26914a74e9834d3dba3677d77cbc1
MJZwart <prorenata.blue@gmail.com>
2022-10-12 15:29:04 +0200

    Validation message

--- app/Rules/ExistingAchievementType.php
+++ app/Rules/ExistingAchievementType.php
@@ -35,6 +35,6 @@ class ExistingAchievementType implements Rule
      */
     public function message()
     {
-        return 'The validation error message.';
+        return 'Invalid achievement type.';
     }
 }
