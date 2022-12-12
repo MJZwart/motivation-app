18db414f62c21b6acd2a6c52a5ec326ecde4adb2
MJZwart <prorenata.blue@gmail.com>
2022-12-12 15:59:52 +0100

    Migrations

---
+++ database/migrations/2022_12_12_131640_create_group_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_messages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->longText('message');
+            $table->softDeletes();
+            $table->foreignId('group_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_messages');
+    }
+};
