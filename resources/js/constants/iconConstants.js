0c290dc1cebd7a6612a6b9dd473bc0cd05a394ee
MJZwart <prorenata.blue@gmail.com>
2023-01-02 14:13:33 +0100

    Setup and navigation

--- resources/js/constants/iconConstants.js
+++ resources/js/constants/iconConstants.js
@@ -1,2 +1,9 @@
 export const ARROW_UP = 'material-symbols:keyboard-arrow-up-rounded';
 export const ARROW_DOWN = 'material-symbols:keyboard-arrow-down-rounded';
+
+export const NOTIFICATION_UNREAD = 'carbon:notification-new';
+// export const NOTIFICATION = 'carbon:notification';
+
+export const MAIL = 'bi:envelope-fill';
+export const NOTIFICATION = 'mdi:bell';
+export const DOT = 'carbon:dot-mark';
