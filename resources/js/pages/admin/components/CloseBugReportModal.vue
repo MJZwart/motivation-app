0fc180ce49e5a50c4d745a05a0f46286a69493dd
MJZwart <prorenata.blue@gmail.com>
2023-05-31 13:12:58 +0200

    Bug Report

--- resources/js/pages/admin/components/CloseBugReportModal.vue
+++ resources/js/pages/admin/components/CloseBugReportModal.vue
@@ -5,33 +5,29 @@
         <p><b>{{ $t('severity') }}</b>: {{ bugReport.severity }}</p>
         <p><b>{{ $t('comment') }}</b>: {{ bugReport.comment }}</p>
     </div>
-    <form @submit.prevent="emit('submit', adminComment)">
-        <SimpleTextarea 
-            id="admin-comment" 
-            v-model="adminComment"
-            type="text" 
-            name="admin_comment" 
-            :label="$t('admin-comment')"
-            :placeholder="adminComment" />
-        <small class="form-text text-muted">{{$t('close-bug-admin-comment-desc')}}</small>
-        <div class="d-flex">
-            <SubmitButton class="ml-auto mr-1">{{ $t('close-bug-report') }}</SubmitButton>
-            <button class="cancel-button">{{ $t('cancel') }}</button>
-        </div>
-    </form>
+    <SimpleTextarea 
+        id="admin-comment" 
+        v-model="bugReport.admin_comment"
+        type="text" 
+        name="admin_comment" 
+        :label="$t('admin-comment')"
+        :placeholder="bugReport.admin_comment" />
+    <small class="form-text text-muted">{{$t('close-bug-admin-comment-desc')}}</small>
+    <div class="d-flex">
+        <SubmitButton class="ml-auto mr-1" @click="$emit('submit', bugReport)">{{ $t('close-bug-report') }}</SubmitButton>
+        <button class="cancel-button">{{ $t('cancel') }}</button>
+    </div>
 </template>
 
 <script setup lang="ts">
 import SubmitButton from '/js/components/global/small/SubmitButton.vue';
 import {BugReport} from 'resources/types/bug';
-import {onMounted, ref} from 'vue';
+import {ref} from 'vue';
+import {deepCopy} from '/js/helpers/copy';
 
-const adminComment = ref('');
+const props = defineProps<{form: BugReport}>();
+defineEmits(['close', 'submit']);
 
-onMounted(() => {
-    adminComment.value = props.bugReport.admin_comment ?? '';
-});
+const bugReport = ref(deepCopy(props.form));
 
-const props = defineProps<{bugReport: BugReport}>();
-const emit = defineEmits(['close', 'submit']);
 </script>
