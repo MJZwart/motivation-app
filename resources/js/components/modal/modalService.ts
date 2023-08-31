663386627190ed1d4ad61340a7b4926a80d81b3e
Mitchell Zwart <mitchellzwart91@gmail.com>
2023-08-31 10:03:05 +0200

    Removed console logs

--- resources/js/components/modal/modalService.ts
+++ resources/js/components/modal/modalService.ts
@@ -19,7 +19,6 @@ export function formModal<T = unknown>(
     // eslint-disable-next-line no-unused-vars
     submitEvent: (edited: T) => Promise<void> | void, 
     title: string) {
-    console.log('making a form modal')
     const modal = {
         form: form,
         component: markRaw(component),
@@ -27,7 +26,6 @@ export function formModal<T = unknown>(
         title: title,
     }
     clearErrors();
-    console.log('formModal', modal)
     // @ts-ignore
     modals.value.push({component: markRaw(FormModalVue), modal: modal});
 }
@@ -36,14 +34,12 @@ export function showModal<T = unknown>(
     props: T,
     component: Component,
     title: string) {
-    console.log('making a show modal')
     const modal = {
         props: props,
         component: markRaw(component),
         title: title,
     }
     clearErrors();
-    console.log('showModal', modal)
     modals.value.push({component: markRaw(ShowModalVue), modal: modal});
 }
 
